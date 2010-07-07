a142b5dac1c7b2081502b3fa6048e23dac047cbf
wenke <liangwenke8@gmail.com>
2010-07-08 00:09:45 +0800

    fix bug

--- lib/to_xls.rb
+++ lib/to_xls.rb
@@ -12,15 +12,15 @@ class Array
     if options[:only]
       columns = Array(options[:only]).map(&:to_sym)
     else
-      columns = self.first.class.columns - Array(options[:except]).map(&:to_sym)
+      columns = self.first.class.column_names.map(&:to_sym) - Array(options[:except]).map(&:to_sym)
     end
     
     return '' if columns.empty?
     
-    sheet.row(0).concat(columns.collect(&:human_name))
+    sheet.row(0).concat(columns.map(&:to_s).map(&:humanize))
     
     self.each_with_index do |obj, index|
-      sheet.row(index + 1).replace( columns.collect{ |column| obj.send(column.name) } )
+      sheet.row(index + 1).replace( columns.map{ |column| obj.send(column) } )
     end
     
     book.write(xls_report)
